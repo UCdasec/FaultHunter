6805748fdba058ee1946f2c68954a73bfa4532de
Logan Reichling <loganmreichling@gmail.com>
2022-07-27 11:21:33 -0400

    Update tool window title

--- src/main/java/com/afivd/afivd/Main.java
+++ src/main/java/com/afivd/afivd/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 600);
-        stage.setTitle("Automatic Fault Injection Vulnerability Detector");
+        stage.setTitle("FaultHunter: Automatic Fault Injection Vulnerability Detector");
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("lockClipart.png"))));
         stage.setScene(scene);
         stage.show();
